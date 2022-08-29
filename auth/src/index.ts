f4ac20f51ea7602c549e814ddd2b55d151010323
kevin otieno <kevin.otieno@megatherium.solutions>
2022-08-29 17:21:39 +0300

    feat: add boilerplate for routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,20 @@
 import express from 'express'
 import { json } from 'body-parser'
 
+import { currentUserRouter } from './routes/current_user'
+import { signinRouter } from './routes/signin'
+import { signoutRouter } from './routes/signout'
+import { signupRouter } from './routes/signup'
+
 const app = express()
 app.use(json())
+
 const port = 3000
+
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signupRouter)
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
