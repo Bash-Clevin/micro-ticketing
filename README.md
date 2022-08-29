31033dee32acb35e5d9e8f65dc9b223c3b553d6f
kevin otieno <kevin.otieno@megatherium.solutions>
2022-08-29 16:49:42 +0300

    cloud-version

---
+++ README.md
@@ -0,0 +1,4 @@
+gcloud init
+gcloud auth login
+gcloud auth application-default login
+gcloud container clusters get-credentials ticketing-dev
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,12 @@ import { json } from 'body-parser'
 
 const app = express()
 app.use(json())
+
 const port = 3000
+
+app.get('/api/user/currentuser', (req,res) => {
+    res.send('Hello There')
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
