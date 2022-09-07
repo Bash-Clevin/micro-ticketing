4bce841d726823153a2be87b51856780e8989398
kevin otieno <kevin.otieno@megatherium.solutions>
2022-09-07 04:19:26 +0300

    fix: change mongoserver instance

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,25 +1,27 @@
-import { MongoMemoryServer } from 'mongodb-memory-server'
-import mongoose from 'mongoose'
-import { app } from '../app'
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
 
-let mongo: any
+let mongo: any;
 
 beforeAll(async () => {
-  mongo = new MongoMemoryServer()
-  const mongoUri = await mongo.getUri()
+  process.env.JWT_KEY = "asdfsdf";
 
-  await mongoose.connect(mongoUri)
-})
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+});
 
 beforeEach(async () => {
-  const collections = await mongoose.connection.db.collections()
+  const collections = await mongoose.connection.db.collections();
 
   for (let collection of collections) {
-    await collection.deleteMany({})
+    await collection.deleteMany({});
   }
-})
+});
 
 afterAll(async () => {
-  await mongo.stop()
-  await mongoose.connection.close() 
-})
+  await mongo.stop();
+  await mongoose.connection.close();
+});
