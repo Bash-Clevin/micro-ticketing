6e49527496969b2d7d5b14c29f2c996baacc14d2
kevin otieno <kevin.otieno@megatherium.solutions>
2022-09-07 04:39:40 +0300

    feat: do some tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,14 +1,40 @@
-import request from 'supertest'
-import { app } from '../../app'
+import request from "supertest";
+import { app } from "../../app";
 
-it('returns a  201 on successful signup', async () => {
+it("returns a  201 on successful signup", async () => {
   return request(app)
-  .post('/api/users/signup')
-  .send(
-    {
-      email: 'test@test.com',
-      password: 'password'
-    }
-  )
-  .expect(201)
-})
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+});
+
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "testtest.com",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("checks for duplicate emails", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(400);
+});
